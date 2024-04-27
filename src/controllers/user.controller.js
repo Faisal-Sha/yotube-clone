b612613cfb94f883f6d6f9c8fb6ba3703fbd8175
Faisal Shahzad <faisal.shahzad@techfootintl.com>
2024-04-27 16:52:07 +0500

    user controller algo done

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,16 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+
+const registerUser = asyncHandler( async (req, res) => {
+    // TODO: get User datails from frontend
+    // TODO: validations on user data
+    // TODO: check if user is already registered, email, username
+    // TODO: check for images and avatar
+    // TODO: upload images to cloudinary
+    // TODO: create entry in database
+    // TODO: remove password and refresh token from response
+    // TODO: check for user creation
+    // TODO: return user response from server to frontend
+});
+
+export { registerUser };
